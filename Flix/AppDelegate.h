bb7c869d9f8d86c33aca32efa289246328733fee
Chad Franklin <chadellisfranklin@gmail.com>
2020-06-25 12:18:05 -0400

    Initial Commit

---
+++ Flix/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Flix
+//
+//  Created by chad franklin on 6/25/20.
+//  Copyright © 2020 chad franklin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+
+@end
+
---
+++ Flix/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Flix
+//
+//  Created by chad franklin on 6/25/20.
+//  Copyright © 2020 chad franklin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
